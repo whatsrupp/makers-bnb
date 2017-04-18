bc6affc8b0796256722818608a5cbaf111177c54
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-04-18 14:27:57 +0100

    Close #03 Add ability to add new property

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,10 +2,10 @@ class Property
 
   include DataMapper::Resource
 
-  property :id, Serial
-  property :name, String
-  property :location, String
-  property :price, Decimal
-  property :description, String
+  property :id,           Serial
+  property :name,         String
+  property :location,     String
+  property :price,        Decimal
+  property :description,  String
 
 end
