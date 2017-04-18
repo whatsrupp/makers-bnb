577f9469f08ce34de08c65d7ae8eaaf1abc317c9
alicespyglass <alicespyglass@gmail.com>
2017-04-18 15:19:37 +0100

    user account - include BCrypt password validation and changed controller path from user to user/new  [issue 11]

--- spec/helpers/session.rb
+++ spec/helpers/session.rb
@@ -4,6 +4,7 @@ def sign_up
   visit 'users/new'
   fill_in :email, with: 'hello@hello.com'
   fill_in :password, with: 'password123'
+  fill_in :password_confirmation, with: 'password123'
   click_button 'Register'
 end
 
