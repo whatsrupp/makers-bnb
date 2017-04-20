70822a9bca790389ac9db1b7911c37130f8f1dc7
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-04-20 12:53:22 +0100

    Add search functionality for filtering spaces by city

--- app/controllers/filter.rb
+++ app/controllers/filter.rb
@@ -1,10 +1,14 @@
 class MakersBnB < Sinatra::Base
 
+  post '/filter' do
+    @city = params[:location]
+    redirect "/filter/#{@city}"
+  end
+
   get '/filter/:location' do
     filter = Space.all
     @spaces = filter.select! { |space| space.location if space.location == params[:location]}
     erb :'spaces/index'
-
   end
 
 end
